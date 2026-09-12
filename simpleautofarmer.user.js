605f7ab653b67f665435e65223382d556efdacb7
Trambelus <9337657+trambelus@users.noreply.github.com>
2026-09-12 15:32:21 -0400

    Fix [ON]/[OFF] button indicators on load
    It was reading the global plantState instead of the state parameter in the createButton method.

--- simpleautofarmer.user.js
+++ simpleautofarmer.user.js
@@ -67,7 +67,7 @@ function initAutoFarm() {
             button.className = 'btn btn-block btn-' + (state ? 'success' : 'danger');
             button.style.height = '50px';
             button.style.fontSize = '9pt';
-            button.textContent = `Auto ${name[0].toUpperCase() + name.slice(1)}\n[${plantState ? 'ON' : 'OFF'}]`;
+            button.textContent = `Auto ${name[0].toUpperCase() + name.slice(1)}\n[${state ? 'ON' : 'OFF'}]`;
             button.onclick = function() { func(); };
 
             buttonDiv.appendChild(button);
